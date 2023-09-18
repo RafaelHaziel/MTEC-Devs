bb2e9a77556506c48df9096a40a33b2a14bdfe95
Rafael Haziel <hazieldefato@gmail.com>
2023-09-18 08:44:36 -0300

    Criado a classe de Contexto, a ConnectionString e adicionado os serviços de DbContext e Identity ao program

--- MtecDevs/Program.cs
+++ MtecDevs/Program.cs
@@ -1,7 +1,26 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MtecDevs.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Obejtos auxiliares de conexão
+string conn = builder.Configuration.GetConnectionString("MtecDevs");
+var version = ServerVersion.AutoDetect(conn);
+
+
+// Serviço de conexão com o banco de dados - Contexto
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(conn, version)
+);
+
+// Serviço de gestão de usuário - Identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
@@ -18,6 +37,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
